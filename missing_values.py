f1fdc840ed7ee22120edc271ec72e586ebdc003a
Simran Soni <115936849+simran971@users.noreply.github.com>
2023-09-05 21:26:47 +0530

    Add files via upload

---
+++ missing_values.py
@@ -0,0 +1,20 @@
+# Data Preprocessing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, 1:-1].values   #all rows but all except last column
+y = dataset.iloc[:, 3].values   #all rows but only 3rd column
+# print(X)
+# print(y)
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
